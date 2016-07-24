bd9bc9c78aeb820a8395c78aab71909001a5865b
Raymond Lochner <ray_lochner@yahoo.de>
2016-07-24 19:32:55 +0200

    Chess piece images added, drawing the board Adding of the black and white chess pieces. Adding a method in the Window class to draw the current board situation

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -5,11 +5,28 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import board.Board;
+import chess.Chess;
+import pieces.Bishop;
+import pieces.King;
+import pieces.Knight;
+import pieces.Pawn;
+import pieces.PieceColor;
+import pieces.Queen;
+import pieces.Tower;
 
 /*
  * Class responsible to display the chessboard and register the mouse
  */
-public class Window extends Applet implements Runnable {
+public class Window extends Applet implements Runnable, MouseListener {
 
 	private static final long serialVersionUID = 8551786484748424017L;
 	private static int FPS = 60;
@@ -18,10 +35,32 @@ public class Window extends Applet implements Runnable {
 	private static long timeSinceLastUpdate;
 
 	public static int windowWidth = 640;
-	public static int windowHeight = 480;
+	public static int windowHeight = 640;
 
 	private Image image;
 	private Graphics second;
+
+	/*
+	 * black and white piece images
+	 */
+	private BufferedImage img_black_pawn;
+	private BufferedImage img_black_tower;
+	private BufferedImage img_black_knight;
+	private BufferedImage img_black_bishop;
+	private BufferedImage img_black_queen;
+	private BufferedImage img_black_king;
+
+	private BufferedImage img_white_pawn;
+	private BufferedImage img_white_tower;
+	private BufferedImage img_white_knight;
+	private BufferedImage img_white_bishop;
+	private BufferedImage img_white_queen;
+	private BufferedImage img_white_king;
+
+	/*
+	 * board instance
+	 */
+	private Board board;
 
 	@Override
 	public void init() {
@@ -31,6 +70,59 @@ public class Window extends Applet implements Runnable {
 		setFocusable(true);
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Snake");
+
+		loadImages();
+
+		board = new Board();
+		Chess.setupNormalMatch(board);
+
+		this.addMouseListener(this);
+	}
+
+	private void loadImages() {
+		try {
+			URL url_black_pawn = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/pawn_black.png");
+			URL url_black_tower = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/tower_black.png");
+			URL url_black_knight = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/knight_black.png");
+			URL url_black_bishop = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/bishop_black.png");
+			URL url_black_queen = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/queen_black.png");
+			URL url_black_king = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/king_black.png");
+
+			URL url_white_pawn = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/pawn_white.png");
+			URL url_white_tower = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/tower_white.png");
+			URL url_white_knight = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/knight_white.png");
+			URL url_white_bishop = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/bishop_white.png");
+			URL url_white_queen = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/queen_white.png");
+			URL url_white_king = new URL(getCodeBase(),
+					"/home/raymond/Documents/eclipse_workspace/chess/images/king_white.png");
+
+			img_black_pawn = ImageIO.read(url_black_pawn);
+			img_black_tower = ImageIO.read(url_black_tower);
+			img_black_knight = ImageIO.read(url_black_knight);
+			img_black_bishop = ImageIO.read(url_black_bishop);
+			img_black_queen = ImageIO.read(url_black_queen);
+			img_black_king = ImageIO.read(url_black_king);
+
+			img_white_pawn = ImageIO.read(url_white_pawn);
+			img_white_tower = ImageIO.read(url_white_tower);
+			img_white_knight = ImageIO.read(url_white_knight);
+			img_white_bishop = ImageIO.read(url_white_bishop);
+			img_white_queen = ImageIO.read(url_white_queen);
+			img_white_king = ImageIO.read(url_white_king);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -48,7 +140,7 @@ public class Window extends Applet implements Runnable {
 	public void destroy() {
 		super.destroy();
 	}
-	
+
 	@Override
 	public void run() {
 		while (true) {
@@ -60,13 +152,132 @@ public class Window extends Applet implements Runnable {
 				long timeToSlepp = millisPerFrame - timeSinceLastUpdate;
 				try {
 					Thread.sleep(timeToSlepp);
-					//Thread.sleep(16);
+					// Thread.sleep(16);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 			lastUpdate = System.currentTimeMillis();
 		}
+	}
+
+	@Override
+	public void update(Graphics g) {
+
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+
+		second.setColor(Color.gray);
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		/*
+		 * painting the board
+		 */
+		g.setColor(Color.white);
+		boolean flip = true;
+		for (int y = 0; y < 8; y++) {
+			for (int x = 0; x < 8; x++) {
+				if (flip) {
+					g.fillRect(x * 80 + 2, y * 80 + 2, 76, 76);
+				}
+				flip = !flip;
+			}
+			flip = !flip;
+		}
+
+		paintChessPieces(g);
+	}
+
+	/**
+	 * Painting the chess pieces of the board onto the board
+	 * 
+	 * @param g
+	 *            The graphics instance
+	 */
+	private void paintChessPieces(Graphics g) {
+		for (int y = 0; y < 8; y++) {
+			for (int x = 0; x < 8; x++) {
+				if (board.getPieceAt(x, y) != null) {
+					BufferedImage paintPiece = img_white_pawn;
+					/*
+					 * checking all white pieces
+					 */
+					if (board.getPieceAt(x, y).color() == PieceColor.WHITE) {
+						if (board.getPieceAt(x, y).getClass().equals(Pawn.class)) {
+							paintPiece = img_white_pawn;
+						} else if (board.getPieceAt(x, y).getClass().equals(Tower.class)) {
+							paintPiece = img_white_tower;
+						} else if (board.getPieceAt(x, y).getClass().equals(Knight.class)) {
+							paintPiece = img_white_knight;
+						} else if (board.getPieceAt(x, y).getClass().equals(Bishop.class)) {
+							paintPiece = img_white_bishop;
+						} else if (board.getPieceAt(x, y).getClass().equals(Queen.class)) {
+							paintPiece = img_white_queen;
+						} else if (board.getPieceAt(x, y).getClass().equals(King.class)) {
+							paintPiece = img_white_king;
+						}
+						/*
+						 * checking all black pieces
+						 */
+					} else {
+						if (board.getPieceAt(x, y).getClass().equals(Pawn.class)) {
+							paintPiece = img_black_pawn;
+						} else if (board.getPieceAt(x, y).getClass().equals(Tower.class)) {
+							paintPiece = img_black_tower;
+						} else if (board.getPieceAt(x, y).getClass().equals(Knight.class)) {
+							paintPiece = img_black_knight;
+						} else if (board.getPieceAt(x, y).getClass().equals(Bishop.class)) {
+							paintPiece = img_black_bishop;
+						} else if (board.getPieceAt(x, y).getClass().equals(Queen.class)) {
+							paintPiece = img_black_queen;
+						} else if (board.getPieceAt(x, y).getClass().equals(King.class)) {
+							paintPiece = img_black_king;
+						}
+					}
+					g.drawImage(paintPiece, x*80+7, y*80+7, this);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		System.out.println("Registered click at " + (e.getX() / 80) + " " + (e.getY() / 80));
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
